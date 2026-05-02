b7f3f48240bf95dc38d6ee5766839ebf5fee903e
fatiu1525 <muritalafatiumunabau@gmail.com>
2026-05-02 23:19:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -59,5 +59,5 @@ def webhook():
     return jsonify({"status": "ok"}), 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
